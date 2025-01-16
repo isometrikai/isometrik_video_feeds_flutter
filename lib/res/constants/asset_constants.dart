4eda5e075e6c3feeeef5526d51d1cc69132b539d
Asjad <asjad@mobifyi.com>
2025-01-16 12:44:32 +0530

    main assets package issue solved

--- lib/res/constants/asset_constants.dart
+++ lib/res/constants/asset_constants.dart
@@ -1,18 +1,18 @@
 class AssetConstants {
   AssetConstants._();
 
-  static const String icAppBarIcon = 'assets/icons/ic_app_bar_icon.svg';
-  static const String icCrossIcon = 'assets/icons/ic_cross_icon.svg';
-  static const String icRightArrowIcon = 'assets/icons/ic_right_arrow.svg';
-  static const String icBackArrowIcon = 'assets/icons/ic_app_bar_back_icon.svg';
-  static const String icCloseBottomSheet = 'assets/icons/ic_close_bottom_sheet.svg';
+  static const String icAppBarIcon = 'packages/ism_video_reel_player/assets/icons/ic_app_bar_icon.svg';
+  static const String icCrossIcon = 'packages/ism_video_reel_player/assets/icons/ic_cross_icon.svg';
+  static const String icRightArrowIcon = 'packages/ism_video_reel_player/assets/icons/ic_right_arrow.svg';
+  static const String icBackArrowIcon = 'packages/ism_video_reel_player/assets/icons/ic_app_bar_back_icon.svg';
+  static const String icCloseBottomSheet = 'packages/ism_video_reel_player/assets/icons/ic_close_bottom_sheet.svg';
 
-  static const muteRoundedSvg = 'assets/icons/mute_rounded.svg';
-  static const unmuteRoundedSvg = 'assets/icons/unmute_rounded.svg';
-  static const greenTickIcon = 'assets/icons/green_tick.svg';
-  static const verifiedSVG = 'assets/icons/ic_verified.svg';
-  static const heartAnimation = 'assets/json/heart_animation.json';
-  static const pausedRoundedSvg = 'assets/icons/pause_rounded.svg';
-  static const reelsPlaySvg = 'assets/icons/reels_play.svg';
+  static const muteRoundedSvg = 'packages/ism_video_reel_player/assets/icons/mute_rounded.svg';
+  static const unmuteRoundedSvg = 'packages/ism_video_reel_player/assets/icons/unmute_rounded.svg';
+  static const greenTickIcon = 'packages/ism_video_reel_player/assets/icons/green_tick.svg';
+  static const verifiedSVG = 'packages/ism_video_reel_player/assets/icons/ic_verified.svg';
+  static const heartAnimation = 'packages/ism_video_reel_player/assets/json/heart_animation.json';
+  static const pausedRoundedSvg = 'packages/ism_video_reel_player/assets/icons/pause_rounded.svg';
+  static const reelsPlaySvg = 'packages/ism_video_reel_player/assets/icons/reels_play.svg';
   static const postResponseJson = 'packages/ism_video_reel_player/assets/loader/post_response.json';
 }
