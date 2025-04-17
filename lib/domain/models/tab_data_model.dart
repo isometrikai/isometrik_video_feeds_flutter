6e4d39abb107008bdc44b17222e85445ab28c94e
Asjad <asjad@mobifyi.com>
2025-04-17 20:13:18 +0530

    main - passing user Id on click of more

--- lib/domain/models/tab_data_model.dart
+++ lib/domain/models/tab_data_model.dart
@@ -28,7 +28,7 @@ class TabDataModel {
   final List<PostDataModel>? postList;
   final Future<String?> Function()? onCreatePost;
   final Future<List<PostDataModel>> Function()? onLoadMore;
-  final Future<bool> Function(String)? onTapMore;
+  final Future<bool> Function(String postId, String userId)? onTapMore;
   final bool? showBlur;
   final List<FeaturedProductDataItem>? productList;
   final Future<bool> Function(String postId)? onPressSave;
