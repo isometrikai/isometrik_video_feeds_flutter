b206af050ad1788438c582dcd539764d47017881
Asjad <asjad@mobifyi.com>
2025-10-23 13:59:10 +0530

    video scrolling optimization

--- lib/presentation/screens/posts/ism_reels_video_player_view.dart
+++ lib/presentation/screens/posts/ism_reels_video_player_view.dart
@@ -2554,35 +2554,3 @@ class _IsmReelsVideoPlayerViewState extends State<IsmReelsVideoPlayerView>
     });
   }
 }
-
-class TrianglePainter extends CustomPainter {
-  TrianglePainter({this.color = Colors.white});
-
-  final Color color;
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = color
-      ..style = PaintingStyle.fill;
-
-    final path = Path()
-      ..moveTo(size.width / 2, size.height)
-      ..lineTo(0, 0)
-      ..lineTo(size.width, 0)
-      ..close();
-
-    canvas.drawPath(path, paint);
-
-    // Add shadow
-    canvas.drawShadow(
-      path,
-      Colors.black.changeOpacity(0.2),
-      2.0,
-      false,
-    );
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
-}
